fc77f1eedb725cd91ba194249b768338dab70938
DEV1lmig <maquiroz@urbe.edu.ve>
2023-10-04 17:17:14 -0400

    second commit

--- src/lib/componets/FormCard.svelte
+++ src/lib/componets/FormCard.svelte
@@ -1,14 +1,21 @@
 <script>
     import { fixMyEnglish } from '../fixMyEnglish'
     let promise = null
+    let minleng = 5
 
     const handleClick = async () => {
         const text = document.getElementById("result").value
-        promise = fixMyEnglish(text)
+        const length = text.length
+        if (length > minleng){
+            promise = fixMyEnglish(text)
+        }else{
+            promise = "El texto tiene que ser mayor a 5 caracteres"
+        }
         const value = await promise
 
         document.getElementById("result").value = value
     }
+
 
     const resetButton = async () => {
         promise = await null
@@ -17,27 +24,26 @@
 </script>
 
 <div class="container max-w-lg m-auto flex flex-col gap-4">
-  <textarea id="result" class="textarea textarea-bordered" placeholder="Put your english text here" cols=70></textarea>
-  
-  {#if promise === null}
-<button
-on:click={handleClick}
-type="button" 
-class="btn btn-primary"
->Fix it</button>
-{:else}
+  <textarea id="result" class="textarea textarea-bordered" placeholder="Introduce tu texto en ingles" cols=70></textarea>
 
-{#await promise}
-<button class="btn btn-disable">
-    <span class="loading loading-spinner"></span>
-    loading...
-  </button>
-{:then}
-    {#if promise != null} 
-        <button class="btn btn-success">Success</button>
-        <button on:click={resetButton} class="py-4 btn btn-success">Reset</button>
+    {#if promise === null}
+    <button
+    on:click={handleClick}
+    type="button" 
+    class="btn btn-primary"
+    >Arreglalo</button>
+    {:else}
+
+    {#await promise}
+    <button class="btn btn-disable">
+        <span class="loading loading-spinner"></span>
+        cargando...
+    </button>
+    {:then}
+        {#if promise != null} 
+            <button class="btn btn-success">Completado</button>
+            <button on:click={resetButton} class="py-4 btn btn-success">Resetear</button>
+        {/if}
+    {/await}
     {/if}
-{/await}
-{/if}
-    
 </div>
