c65268936f3222658f49348950d0e4ad7dd4c954
DEV1lmig <maquiroz@urbe.edu.ve>
2023-10-11 00:59:23 -0400

    added mail verification

---
+++ src/routes/auth/+page.server.js
@@ -0,0 +1,12 @@
+/** @type {import('./$types').PageServerLoad} */
+
+import { redirect } from '@sveltejs/kit';
+
+export const actions={
+    verification: async ({ locals }) => {
+        const email = locals.userPb.authStore.model.email;
+        const res = await locals.userPb.collection('users').requestVerification(email)
+        
+        if(res) throw redirect(303, '/login')
+    }
+}
--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -3,34 +3,26 @@
 import { redirect } from '@sveltejs/kit';
 
 export const actions = {
-    login: async({cookies,request,locals})=>{
+	login: async ({ cookies, request, locals }) => {
+		const loginFormData = await request.formData();
+		const email = loginFormData.get('email')?.toString() ?? '';
+		const password = loginFormData.get('password')?.toString() ?? '';
 
-        const loginFormData = await request.formData();
-        const email = loginFormData.get('email')?.toString()??'';
-        const password = loginFormData.get('password')?.toString()??'';
+		let loginResponse = {
+			email,
+			error: true,
+			message: ''
+		};
 
-        let loginResponse={
-            email,
-            error: true,
-            message: '',
-        }
-
-        try{
-            await locals.userPb.collection('users').authWithPassword(
-                email,
-                password
-            );
-            //console.log('auth store',locals.pb.authStore);
- 
-            
-            if(locals.userPb.authStore.baseToken) throw redirect(303,'/spelly')
-        }
-        finally{
-            if(!locals.userPb.authStore.baseToken) return loginResponse
-        }
-
-
-        
-    },
-
-}
+		try {
+			await locals.userPb.collection('users').authWithPassword(email, password);
+			//console.log('auth store',locals.pb.authStore);
+			if (!locals.userPb?.authStore?.model?.verified){
+				throw redirect(303, '/auth')
+			}
+			if (locals.userPb.authStore.baseToken) throw redirect(303, '/spelly');
+		} finally {
+			if (!locals.userPb.authStore.baseToken) return loginResponse;
+		}
+	}
+};
